72cca1a5dc4cd0ca7674864e6e0e6ba08bd71ce9
壹刀流 <idevebi@163.com>
2020-08-19 13:48:51 +0800

    feat: login route

--- imooc/dell-ts-react-demo/src/App.tsx
+++ imooc/dell-ts-react-demo/src/App.tsx
@@ -1,54 +1,13 @@
 import React from 'react'
-import { Form, Input, Button } from 'antd'
-import { UserOutlined, LockOutlined } from '@ant-design/icons'
-import './login.css'
+import { HashRouter, Switch, Route } from 'react-router-dom'
+import LoginPage from './pages/Login'
 
-interface LoginFormData {
-  username: string
-  password: string
-}
-
-export default () => {
-  const onFinish = (values: any) => {
-    const data: LoginFormData = values
-
-    console.log('表单数据：', data.username, data.password)
-  }
-
-  return (
-    <div id="login-page">
-      <Form name="normal_login" className="login-form" onFinish={onFinish}>
-        <Form.Item
-          name="username"
-          rules={[{ required: true, message: '请输入用户名。' }]}
-        >
-          <Input
-            prefix={<UserOutlined className="site-form-item-icon" />}
-            placeholder="用户名"
-            autoComplete="username"
-          />
-        </Form.Item>
-        <Form.Item
-          name="password"
-          rules={[{ required: true, message: '请输入密码。' }]}
-        >
-          <Input
-            prefix={<LockOutlined className="site-form-item-icon" />}
-            type="password"
-            placeholder="密码"
-            autoComplete="current-password"
-          />
-        </Form.Item>
-        <Form.Item>
-          <Button
-            type="primary"
-            htmlType="submit"
-            className="login-form-button"
-          >
-            登录
-          </Button>
-        </Form.Item>
-      </Form>
-    </div>
-  )
-}
+export default () => (
+  <div>
+    <HashRouter>
+      <Switch>
+        <Route path="/login" exact component={LoginPage} />
+      </Switch>
+    </HashRouter>
+  </div>
+)
