1b96054fbc92c8e9bfba3c40663780fd9287e642
壹刀流 <idevebi@163.com>
2020-08-19 13:21:22 +0800

    feat: login page

--- imooc/dell-ts-react-demo/src/App.tsx
+++ imooc/dell-ts-react-demo/src/App.tsx
@@ -1,3 +1,53 @@
 import React from 'react'
+import { Form, Input, Button } from 'antd'
+import { UserOutlined, LockOutlined } from '@ant-design/icons'
+import './login.css'
 
-export default () => <div>Hello React</div>
+export default () => {
+  const onFinish = (values: any) => {
+    console.log('Received values of form: ', values)
+  }
+
+  return (
+    <div id="login-page">
+      <Form
+        name="normal_login"
+        className="login-form"
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+      >
+        <Form.Item
+          name="username"
+          rules={[{ required: true, message: '请输入用户名。' }]}
+        >
+          <Input
+            prefix={<UserOutlined className="site-form-item-icon" />}
+            placeholder="用户名"
+            autoComplete="username"
+          />
+        </Form.Item>
+        <Form.Item
+          name="password"
+          rules={[{ required: true, message: '请输入密码。' }]}
+        >
+          <Input
+            prefix={<LockOutlined className="site-form-item-icon" />}
+            type="password"
+            placeholder="密码"
+            autoComplete="current-password"
+          />
+        </Form.Item>
+
+        <Form.Item>
+          <Button
+            type="primary"
+            htmlType="submit"
+            className="login-form-button"
+          >
+            登录
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  )
+}
