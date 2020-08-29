d77af6a72a204562a39f72ceb9bf894b7474ea57
壹刀流 <idevebi@163.com>
2020-08-29 18:46:29 +0800

    feat: news-spider get raw html

--- news-spider/src/index.ts
+++ news-spider/src/index.ts
@@ -1,1 +1,22 @@
-console.log('news spider')
+import axios from 'axios'
+
+class NewsSpider {
+  /// 爬取网站地址
+  private url = 'http://news.163.com/'
+
+  /** 开始爬取新闻排行榜 */
+  startSpiderTopNews() {
+    const html = this.getRawHTML()
+  }
+
+  /** 获取原始 HTML 内容 */
+  private async getRawHTML() {
+    const result = await axios.get(this.url)
+    console.log(result.data.toString())
+
+    return result.data.toString()
+  }
+}
+
+const spider = new NewsSpider()
+spider.startSpiderTopNews()
